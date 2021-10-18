c296b7c2b7dbbd59355a92f4ce3dd58efded13f0
Bryan Brickman <bryanlbrick@gmail.com>
2021-10-18 13:57:36 -0500

    Adds case ID, adds spacing, reduces amount of alter info

--- src/main.py
+++ src/main.py
@@ -1,58 +1,71 @@
 import reportlab.pdfgen.canvas
 import pandas as pd
 
-import os
 import re
+import os
 
+path_to_data_dir = "fake_test_data/"
+if path_to_data_dir[len(path_to_data_dir)-1] != '/':  # add a backslash to path name if omitted
+    path_to_data_dir = path_to_data_dir + '/'
+    print(path_to_data_dir)
 
-path_to_data_dir = "fake_test_data/"  # make sure to end the path with a "/"
-
-all_files = os.listdir(path_to_data_dir)
-
-relevant_files = filter(lambda x: x.__contains__("_attributeList_Person.csv"), all_files)  # yields a filter object
-
-relevant_files = [*relevant_files]  # unpacks the filter object into a list of file names
-
-# TODO gather correct alter attributes from data
 # define alter attributes and attribute block line length
 name = "NAME"
 gender = "GENDER"
 relationship = "RELATIONSHIP"
-communication_frequency = "COMMUNICATION_FREQUENCY"
-discussed_su_in_last_30_days = "DISCUSSED SU IN LAST 30 DAYS"
+# communication_frequency = "COMMUNICATION_FREQUENCY"
+# discussed_su_in_last_30_days = "DISCUSSED SU IN LAST 30 DAYS"
 
-attributes = {'name': name, 'gender': gender, 'relationship': relationship,
-              'communication_frequency': communication_frequency,
-              'discussed_su_in_last_30_days': discussed_su_in_last_30_days}
+attributes = {'name': name, 'gender': gender, 'relationship': relationship, "": ""}
 
-BLOCK_LINE_LENGTH = len(attributes)
+              # 'communication_frequency': communication_frequency,
+              # 'discussed_su_in_last_30_days': discussed_su_in_last_30_days}
 
-
+BLOCK_LINE_LENGTH = len(attributes)  # the length (in lines) of the alter attributes block
 left_margin = 25
 top_margin = 50
-# US_letter_dimensions =
-page_begin_y = 841
+page_begin_y = 841  # this assumes the default reportlab size (A4)
 font_size = 12
 
 
-def draw_line(canvas, tab_level, category, string, index):
+def get_relevant_files(path):
+    """ grabs only the exported Net. Canvas files that contain alter data (*_attributeList_Person.csv) """
+    path_to_data_dir = path
+    all_files = os.listdir(path_to_data_dir)
+    relevant_files = filter(lambda x: x.__contains__("_attributeList_Person.csv"), all_files)  # yields a filter object
+    relevant_files = [*relevant_files]  # unpacks the filter object into a list of file names
+    return relevant_files
+
+
+def draw_ego_id(canvas, ego_id):
+    """ draws the ego information """
+    canvas.drawString(left_margin, page_begin_y - top_margin, f'Case ID: {ego_id[0]}')
+    canvas.drawString(left_margin, page_begin_y - top_margin - 10, f'UUID: {ego_id[1]}')
+    canvas.drawString(left_margin, page_begin_y - top_margin - 20, f'')
+
+
+
+def draw_line(canvas, tab_level, category, string, index, kind):
     """ draws a line of text to the canvas at a specified tab level, with vertical offset """
-    canvas.drawString(left_margin + (12 * (tab_level + 1)), page_begin_y - top_margin - 15 - (12 * index), f'{category}: {string}')
+    # a bunch of hardcoded, undocumented offsets hacked together
+    if kind == "attribute":
+        canvas.drawString(left_margin + (12 * (tab_level + 1)), page_begin_y - top_margin - 15 - 20 -(12 * index), f'{category}: {string}')
 
-
-def draw_ego_ID(canvas, ego_id):
-    canvas.drawString(left_margin, page_begin_y - top_margin, f'ID: {ego_id}')
-
+    if kind == "space":
+        canvas.drawString(left_margin + (12 * (tab_level + 1)), page_begin_y - top_margin - 15 - 20 -(12 * index), f'')
 
 def draw_alter(canvas, category_list, value_list, number, block_line_length):
     """ draws formatted lines of data for a single alter """
     # alter name
-    draw_line(canvas, 0, category_list[0], value_list[0], number * block_line_length)
+    # draw_line(canvas, 1, "", "", number * block_line_length)
+    draw_line(canvas, 0, category_list[0], value_list[0], number * block_line_length, kind="attribute")
 
     # alter attributes
     for i in range(len(category_list)):
-        if i != 0:
-            draw_line(canvas, 1, category_list[i], value_list[i], i + number * block_line_length)
+        if i != 0 and i != len(category_list) - 1:
+            draw_line(canvas, 1, category_list[i], value_list[i], i + number * block_line_length, kind="attribute")
+        else:
+            draw_line(canvas, 1, category_list[i], value_list[i], i + number * block_line_length, kind="space")
 
 
 def draw_all_alters(canvas, num_alters, category_list, all_alter_values, block_line_length):
@@ -61,27 +74,28 @@ def draw_all_alters(canvas, num_alters, category_list, all_alter_values, block_l
         draw_alter(canvas, category_list, all_alter_values[i], i, block_line_length)
 
 
+def strip_brackets(name):
+    return name.strip('[]')
+
+
 def get_attributes(alters_dataframe, entry):
+    """ Gathers the attributes of each alter. Define the desired attributes by modifying "attributes" at the end of
+    the function """
     # print(alters_dataframe[entry:entry+1])
     data = alters_dataframe[entry:entry+1]
 
-    print(data)
-    # attributes needed for report
+    # define attributes needed for report
     name = "NAME"
     gender = "GENDER"
     relationship = "RELATIONSHIP"
     communication_frequency = "COMMUNICATION_FREQUENCY"
     discussed_su_in_last_90_days = "DISCUSSED SU IN LAST 30 DAYS"
 
-    attributes = {'name': name, 'gender': gender, 'relationship': relationship,
-                  'communication_frequency': communication_frequency,
-                  'discussed_su_in_last_90_days': discussed_su_in_last_90_days}
-
 
     # collect attributes (this is a bit annoying, at least the way I do it)
-    name = data['name'].values
-    # gender = data["gender_m"]
-    # print(gender.values)
+    name = list(data['name'].values)
+    name = name[0]  # this strips the brackets from the alter names
+
     if data['gender_m'].values[0]:
         gender = "gender_m"
     if data['gender_f'].values[0]:
@@ -94,6 +108,8 @@ def get_attributes(alters_dataframe, entry):
         gender = "gender_gnc"
     if data['gender_dk'].values[0]:
         gender = "gender_dk"
+    if data['gender_refuse'].values[0]:
+        gender = "gender_refuse"
 
     if data['relationship_type_partner'].values[0]:
         relationship = "relationship_type_partner"
@@ -120,27 +136,28 @@ def get_attributes(alters_dataframe, entry):
     if data['relationship_type_o'].values[0]:
         relationship = "relationship_type_o"
 
-    communication_frequency = data['communication_frequency'].values[0]
-    if communication_frequency == 0:
-        communication_frequency = "refused"
-    if communication_frequency == 1:
-        communication_frequency = "daily"
-    if communication_frequency == 2:
-        communication_frequency = "weekly"
-    if communication_frequency == 3:
-        communication_frequency = "monthly"
-    if communication_frequency == 4:
-        communication_frequency = "less than monthly"
-    if communication_frequency == 5:
-        communication_frequency = "never"
+    # communication_frequency = data['communication_frequency'].values[0]
+    # if communication_frequency == 0:
+    #     communication_frequency = "refused"
+    # if communication_frequency == 1:
+    #     communication_frequency = "daily"
+    # if communication_frequency == 2:
+    #     communication_frequency = "weekly"
+    # if communication_frequency == 3:
+    #     communication_frequency = "monthly"
+    # if communication_frequency == 4:
+    #     communication_frequency = "less than monthly"
+    # if communication_frequency == 5:
+    #     communication_frequency = "never"
 
-    if data['discuss_substance'].values[0]:
-        discussed_su_in_last_90_days = True
+    # if data['discuss_substance'].values[0]:
+    #     discussed_su_in_last_90_days = True
 
 
-    attributes = {'name': name, 'gender': gender, 'relationship': relationship,
-                  'communication_frequency': communication_frequency,
-                  'discussed_su_in_last_90_days': discussed_su_in_last_90_days}
+    attributes = {'name': name, 'gender': gender, 'relationship': relationship, "space": "space"}
+                  # 'communication_frequency': communication_frequency,
+                  # 'discussed_su_in_last_90_days': discussed_su_in_last_90_days}
+    print(attributes)
 
     return attributes
 
@@ -149,34 +166,41 @@ def process_interview_responses(alter_data):  # input: pandas DF
     """ intakes alter data and outputs list of dicts, each containing one alter's attributes """
     attribute_list = []
     for i in range(len(alter_data.index)):
-        # attribute_list.append({"name": "NAME"})
         attribute_list.append(get_attributes(alter_data, i))
-        # print(f'attribute list: {attribute_list}')
     return attribute_list
-    # return [{'alter name': 'NAME', 'alter gender': 'GENDER', 'alter relationship': 'REL'}, {'alter name': 'NAME', 'alter gender': 'GENDER', 'alter relationship': 'REL'}]
 
 
-def create_report_for_interview(path, report_number):
+def create_report_for_interview(path):
     """ creates and saves a PDF of alter data for a single ego """
     alter_data = pd.read_csv(path)
     attributes_list: list = process_interview_responses(alter_data)
     BLOCK_LINE_LENGTH = len([*attributes_list[0].keys()])  # number of attributes per alter. This allows us to space alter data blocks
     num_alters = len(attributes_list)
-    # print(f'n alters: {num_alters}')
-    # grab the ego UUID
-    separated_path = re.split(",|_", path)
-    separated_path = [a for a in separated_path if len(a) > 32]  # grab the ego UUID (will not work with folder names over 32 characters in length)
-    ego_UUID = f'{separated_path}'  # I've left this in this format in case the file path exceeds 32 characters- at least it will still contain the UUID in that case
-    canvas = reportlab.pdfgen.canvas.Canvas(f'{ego_UUID}_report.pdf')  # UUID at the beginning for easy searches
-    attribute_name_list = [*attributes_list[0].keys()]
-    # print(f'attr names: {attribute_name_list}')
-    attribute_values_list = [[*attributes.values()] for attributes in attributes_list]
-    # print(f'attr values: {attribute_values_list}')
 
-    draw_ego_ID(canvas, ego_UUID)
+    # grab the ego UUID
+    separated_path = re.split(",|_|/", path)
+    # print(f'sep. path: {separated_path}')
+    # separated_path = [a for a in separated_path if len(a) > 32]  # grab the ego UUID (will not work with folder names over 32 characters in length)
+    ego_UUID = f'{separated_path[len(separated_path) - 1 - 2]}'  # any NC export will place the ego UUID in the 3rd from the end when split as above
+    ego_case_ID = f'{separated_path[len(separated_path) - 1 - 3]}'
+    ego_combined_ID = [ego_case_ID, ego_UUID]
+
+    attribute_name_list = [*attributes_list[0].keys()]
+    attribute_values_list = [[*attributes.values()] for attributes in attributes_list]
+
+    # create the reportlab canvas, draw to it, and save as pdf
+    canvas = reportlab.pdfgen.canvas.Canvas(f'{ego_case_ID + "_" +  ego_UUID}_report.pdf')  # UUID at the beginning for easy searches
+    draw_ego_id(canvas, ego_combined_ID)
     draw_all_alters(canvas, num_alters, attribute_name_list, attribute_values_list, BLOCK_LINE_LENGTH)
     canvas.showPage()
     canvas.save()
 
-for i in range(len(relevant_files)):
-    create_report_for_interview(path_to_data_dir + relevant_files[i], i)
+
+def main():
+    relevant_files = get_relevant_files(path_to_data_dir)
+    for i in range(len(relevant_files)):
+        create_report_for_interview(path_to_data_dir + relevant_files[i])
+
+
+if __name__ == "__main__":
+    main()
